c2156cb7306c9491ecf71413fb6f7f95fbe9fa26
kgathuru <kgathuru@c79eb08e-251e-0410-91f9-d783734ae05e>
2006-10-20 20:10:34 +0000

    removed compiled object files

--- asteroids/object.cpp
+++ asteroids/object.cpp
@@ -1,1 +1,24 @@
 #include "object.h"
+
+//object constructor
+model::object::object(){
+
+}
+
+//speed mutator method 
+void model::object::setSpeed(){
+
+}
+
+//speed accessor method
+double model::object::getSpeed(){
+
+}
+
+void model::object::setDirection(){
+
+}
+
+double model::object::getDirection(){
+
+}
