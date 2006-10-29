7def1f25373bdd3bad0958dbfec56a4592cb28c4
kargatane <kargatane@c79eb08e-251e-0410-91f9-d783734ae05e>
2006-10-29 22:58:48 +0000

    asteroids

--- asteroids/asteroid.cpp
+++ asteroids/asteroid.cpp
@@ -1,7 +1,23 @@
-#include "object.cpp"
+#include "object.h"
+#include "math.h"
 
-namespace model;
+asteroid::asteroid()
+{
+	//Set speed to all be the same
+	speed = 10.0;
+	//Set direction to all be the same
 
-class asteroid: public object {
-	
+	//Set the spin to all be zero by default
+	spin.set(0.0,0.0,0.0);
+}
+
+
+void setSpin(float Sx, float Sy, float Sz)
+{
+	spin.set(Sx,Sy,Sz);
+}
+
+Vector3 getSpin()
+{
+	return spin;
 }
