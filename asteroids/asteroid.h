3c524d84609962fed99a905f70b485a9bb468354
kargatane <kargatane@c79eb08e-251e-0410-91f9-d783734ae05e>
2006-10-29 23:00:18 +0000

    add

---
+++ asteroids/asteroid.h
@@ -0,0 +1,15 @@
+#include "object.h"
+#include "asteroid.h"
+#include "math.h"
+
+namespace model{
+class asteroid: public object{
+   private:
+	Vector3 spin;
+	
+   public:
+	asteroid(); 		// constructor
+	void setSpin(float Sx, float Sy, float Sz);	//Set the spin in degrees
+	Vector3 getSpin();	//Return spin
+};
+};
