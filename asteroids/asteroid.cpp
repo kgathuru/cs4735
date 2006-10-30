b4a9c2ae8f2979ea781d9340947b6f93d6d6084d
kargatane <kargatane@c79eb08e-251e-0410-91f9-d783734ae05e>
2006-10-30 01:04:10 +0000

    asteroid

--- asteroids/asteroid.cpp
+++ asteroids/asteroid.cpp
@@ -3,6 +3,7 @@
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
+#include "mathlib.h"
 
 //Constructor
 asteroid::asteroid()
@@ -11,7 +12,7 @@ asteroid::asteroid()
 	float sizeLimitUpper = 100;	//Upper size limit of asteroid
 	float sizeLimitLower = 1; 	//Lower size limit of asteroid
 	float x,y,z;
-	float 
+	float WIDTH, DEPTH, HEIGHT;
  
 	//Set speed to be the same for every asteroid
 	speed = 10.0;
@@ -46,6 +47,41 @@ asteroid::~asteroid()
     this = NULL;
 }
 
+void asteroid:: recreate()
+{
+	//Size is radius of the sphere that represents the asteroid
+	float sizeLimitUpper = 100;	//Upper size limit of asteroid
+	float sizeLimitLower = 1; 	//Lower size limit of asteroid
+	float x,y,z;
+	float WIDTH, DEPTH, HEIGHT;
+ 
+	//Set speed to be the same for every asteroid
+	speed = 10.0;
+	//Set direction to be the same for every asteroid
+	//Assuming direction is a Vector3
+	direction.set(1.0,0.0,0.0);	//All move in the positive x direction
+
+	//Generate random size between upper and lower limit
+	srand((unsigned)time(0));
+	size = (rand()%sizeLimitUpper)+sizeLimitLower; 
+
+	//Generate random spin for x, y, and z direction
+	//Value in degrees, between 0 and 360 degrees
+	x = (rand()%360);
+	y = (rand()%360);
+	z = (rand()%360);
+	spin.set(x,y,z);
+
+	//Set position randomly based on size of the world
+	//Assuming that world path is the x axis
+	x = (rand()%WIDTH);
+	if(z%x == 0)
+		x = x*(-1)	
+	y = (rand()%HEIGHT);
+	z = (rand()%DEPTH);
+	position.set(x,y,z);
+}
+
 
 void asteroid:: setSpin(float Sx, float Sy, float Sz)
 {
@@ -67,23 +103,30 @@ void asteroid:: doStep(float t)
 	z = position.z + t * direction.z;
 	position.set(x,y,z);
 
+	if(position.z < 0.0)
+	{
+		//Destroy asteroid
+		destroy()
+		//Reset fields and restart at end of path
+		recreate();
+	}
+
 	//Need to check for collision with spaceship
 	//Asteroids can't collide with each other, only ship
 	
-
-
-	//Draw new asteroid
-	draw();
-	
+	//Draw new asteroid after collision detection
 }
 
 void asteroid:: draw()
 {
+	//Rotate the asteroid by how much
+	float angleRot = 5;
+
 	//DRAW NEW ASTEROID
 	glMatrixMode(GL_MODELVIEW);
 
 	//Rotate mesh based on movement
-	glRotatef(t, spin.x, spin.y, spin.z);
+	glRotatef(angleRot, spin.x, spin.y, spin.z);
 
 	//Move asteroid to position in space
 	glTranslatef(position.x, position.y, position.z);
@@ -98,17 +141,35 @@ void asteroid:: draw()
 void asteroid:: destroy()
 {
 	//Animation to remove asteroid
-
-	//object removed
-	this = NULL;
+	for(float i = size; i > 0; i--);
+	{
+		size--;
+		draw();
+	}
 }
 
 bool asteroid:: checkCollision(Point3 shipPosition, float shipSize)
 {
 	//Find distance between two centers
-	
+	float distance;
+	float dx, dy, dz;
+	dx = postion.x - shipPosition.x;
+	dy = postion.y - shipPosition.y;
+	dz = postion.z - shipPosition.z;
+	distance = sqrt(dx*dx + dy*dy + dz*dz);
 
 	//See if it is less than the sum of the raduis of the ship and asteroid
-
-	
+	if(distance <= shipSize + size)
+	{
+		//Need to destroy the asteroid if it's true
+		destroy();
+		//call deconstructor
+		~asteroid();
+		
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
