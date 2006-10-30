fecec5662652fe4727fc8235ad592c2b093b0dea
kgathuru <kgathuru@c79eb08e-251e-0410-91f9-d783734ae05e>
2006-10-30 01:00:37 +0000

    added constants file

---
+++ asteroids/world.h
@@ -0,0 +1,25 @@
+#ifndef WORLD_H
+#define WORLD_H
+
+#include "math.cpp"
+#include "object.cpp"
+
+namespace model{
+
+class world{
+	public:
+	Vector3 pathStart();
+	Vector3 pathEnd();
+	object testpawn;
+	asteroid 
+	double height();
+	double width();
+	double depth();
+	/** \todo add more methods from UML */ 	
+	world();	
+	private:
+};
+
+};
+
+#endif
