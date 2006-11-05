cb387e61b6830f8f7851a84eeaa2f1b9bbf48415
kgathuru <kgathuru@c79eb08e-251e-0410-91f9-d783734ae05e>
2006-11-05 22:43:21 +0000

    world axes

--- asteroids/world.cpp
+++ asteroids/world.cpp
@@ -14,12 +14,38 @@ model::world::~world(){
 	//this = NULL;
 }
 
+/** renders the objects in the world */
 void model::world::render(){
+	/** render axes */
+	glClear(GL_COLOR_BUFFER_BIT);
+	// Draw the axes.
+	glColor3f(1.0, 0.0, 0.0);  
+	glBegin(GL_LINES);  // X axis (red).
+		glVertex3f(0.0, 0.0, 0.0);
+		glVertex3f(5.0, 0.0, 0.0);
+	glEnd();
+	glColor3f(0.0, 1.0, 0.0);  
+	glBegin(GL_LINES);  // Y axis (green).
+		glVertex3f(0.0, 0.0, 0.0);
+		glVertex3f(0.0, 5.0, 0.0);
+	glEnd();
+	glColor3f(0.0, 0.0, 1.0);  
+	glBegin(GL_LINES);  // Z axis (blue).
+		glVertex3f(0.0, 0.0, 0.0);
+		glVertex3f(0.0, 0.0, 5.0);
+	glEnd();
+
+	
 	/** render asteroids */
 	for (asteroid_iterator iter=asteroids.begin(); iter!=asteroids.end(); iter++){
 		iter->readFile("SIMPBARN.3VN");
 		iter->draw();
 	} 
+}
+
+/** updates the objects as time progresses */
+void model::world::update(){
+
 }
 
 /** starting point accessor method */ 
