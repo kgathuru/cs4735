2627e0ca97d3291764c0879f647f62165794f48f
kargatane <kargatane@c79eb08e-251e-0410-91f9-d783734ae05e>
2006-11-06 00:47:48 +0000

    Background framework

--- asteroids/world.cpp
+++ asteroids/world.cpp
@@ -15,62 +15,9 @@ model::world::~world(){
 	//this = NULL;
 }
 
-void model::world::renderBackground(){
-	int i,j;
-	//Set up space box
-	glClear(GL_COLOR_BUFFER_BIT);
-	glDisable(GL_LIGHT0);
-	glColor3f(1.0, 1.0, 1.0);
-	glLineWidth(2);
-
-	glBegin(GL_LINE_STRIP);
-		glVertex3f(-(WIDTH/2), 0.0, 0.0);
-		glVertex3f(WIDTH/2, 0.0, 0.0);
-		glVertex3f(WIDTH/2, HEIGHT, 0.0);
-		glVertex3f(-(WIDTH/2), HEIGHT, 0.0);
-		glVertex3f(-(WIDTH/2), 0.0, 0.0);
-		glVertex3f(-(WIDTH/2), 0.0, DEPTH);
-		glVertex3f(WIDTH/2, 0.0, DEPTH);
-		glVertex3f(WIDTH/2, 0.0, 0.0);
-	glEnd();
-	glBegin(GL_LINE_STRIP);
-		glVertex3f(WIDTH/2, 0.0, DEPTH);
-		glVertex3f(WIDTH/2, HEIGHT, DEPTH);
-		glVertex3f(WIDTH/2, HEIGHT, 0.0);
-		glVertex3f(-(WIDTH/2), HEIGHT, 0.0);
-		glVertex3f(-(WIDTH/2), HEIGHT, DEPTH);
-		glVertex3f(WIDTH/2, HEIGHT, DEPTH);
-		glVertex3f(-(WIDTH/2), HEIGHT, DEPTH);
-		glVertex3f(-(WIDTH/2), 0.0, DEPTH);
-	glEnd();
-	glEnable(GL_LIGHT0);
-
-
-	//Tile top	
-
-
-	//Tile bottom
-
-	//Tile back
-
-	//Tile left
-	for(i = 0; i < HEIGHT/STARHEIGHT; i++)
-	{
-	  for(j = 0; j < DEPTH/STARWIDTH; j++)
-	  {
-		//glViewport(i * STARHEIGHT, j * STARWIDTH, 640/N, 640/N);	
-	  }
-	}
-
-	//Tile right
-
-}
-
 /** renders the objects in the world */
 void model::world::render(){
 	/** render axes */
-	glClear(GL_COLOR_BUFFER_BIT);
-	glDisable(GL_LIGHT0);
 	glColor3f(1.0, 0.0, 0.0);
 	glLineWidth(2);
 	glBegin(GL_LINES);  // X axis (red).
@@ -87,7 +34,28 @@ void model::world::render(){
 		glVertex3f(0.0, 0.0, 0.0);
 		glVertex3f(0.0, 0.0, -DEPTH);
 	glEnd();
-	glEnable(GL_LIGHT0);
+
+	glColor3f(1.0, 1.0, 1.0);
+	glBegin(GL_LINE_STRIP);
+		glVertex3f(-(WIDTH/2), 0.0, 0.0);
+		glVertex3f(WIDTH/2, 0.0, 0.0);
+		glVertex3f(WIDTH/2, HEIGHT, 0.0);
+		glVertex3f(-(WIDTH/2), HEIGHT, 0.0);
+		glVertex3f(-(WIDTH/2), 0.0, 0.0);
+		glVertex3f(-(WIDTH/2), 0.0, -DEPTH);
+		glVertex3f(WIDTH/2, 0.0, -DEPTH);
+		glVertex3f(WIDTH/2, 0.0, 0.0);
+	glEnd();
+	glBegin(GL_LINE_STRIP);
+		glVertex3f(WIDTH/2, 0.0, -DEPTH);
+		glVertex3f(WIDTH/2, HEIGHT, -DEPTH);
+		glVertex3f(WIDTH/2, HEIGHT, 0.0);
+		glVertex3f(-(WIDTH/2), HEIGHT, 0.0);
+		glVertex3f(-(WIDTH/2), HEIGHT, -DEPTH);
+		glVertex3f(WIDTH/2, HEIGHT, -DEPTH);
+		glVertex3f(-(WIDTH/2), HEIGHT, -DEPTH);
+		glVertex3f(-(WIDTH/2), 0.0, -DEPTH);
+	glEnd();
 
 	/** render asteroids */
 	for (asteroid_iterator iter=asteroids.begin(); iter!=asteroids.end(); iter++){
--- asteroids/world.h
+++ asteroids/world.h
@@ -16,7 +16,6 @@ class world{
 	Vector3 getEndPoint();
 	void setEndPoint(Point3 v);
 	void render();
-	void renderBackground();
 	void update();
 	
 	/** STL list of asteroids */
